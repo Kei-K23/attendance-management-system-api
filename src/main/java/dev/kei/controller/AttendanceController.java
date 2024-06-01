c2a391ee559f1cc349d950bbc020a8eb939917f1
Kei-K23 <keiksl2301@gmail.com>
2024-06-01 19:50:42 +0630

    Add CRUD operations for attendance

--- src/main/java/dev/kei/controller/AttendanceController.java
+++ src/main/java/dev/kei/controller/AttendanceController.java
@@ -1,10 +1,13 @@
 package dev.kei.controller;
 
 import dev.kei.dto.AttendanceRequestDto;
+import dev.kei.entity.Attendance;
 import dev.kei.service.AttendanceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/attendance")
@@ -25,5 +28,21 @@ public class AttendanceController {
     public ResponseEntity<Void> clockOut(@RequestBody AttendanceRequestDto attendanceRequestDto) {
         attendanceService.clockOut(attendanceRequestDto);
         return  ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Attendance>> findByUserId(@PathVariable String userId) {
+        return  ResponseEntity.status(HttpStatus.OK).body(attendanceService.findByUserId(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Attendance>> findByUserId() {
+        return  ResponseEntity.status(HttpStatus.OK).body(attendanceService.findAll());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        attendanceService.delete(id);
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 }
--- src/main/java/dev/kei/service/AttendanceService.java
+++ src/main/java/dev/kei/service/AttendanceService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AttendanceService {
@@ -22,80 +23,83 @@ public class AttendanceService {
 
     @Transactional
     public void clockIn(AttendanceRequestDto attendanceRequestDto) {
-        try {
-            List<Attendance> attendances = findByUserId(attendanceRequestDto.getUserId());
-            LocalDateTime currentTime = LocalDateTime.now();
+        List<Attendance> attendances = findByUserId(attendanceRequestDto.getUserId());
+        LocalDateTime currentTime = LocalDateTime.now();
 
-            if(!attendances.isEmpty()) {
-                Attendance lastAttendance = attendances.get(attendances.size() - 1);
-                if(lastAttendance.getClockInTime().toLocalDate().equals(currentTime.toLocalDate())) {
-                    throw new AlreadyMakeCheckInException("Already make check in");
-                }
+        if(!attendances.isEmpty()) {
+            Attendance lastAttendance = attendances.get(attendances.size() - 1);
+            if(lastAttendance.getClockInTime().toLocalDate().equals(currentTime.toLocalDate())) {
+                throw new AlreadyMakeCheckInException("Already make check in");
             }
-
-            Attendance newAttendance = Attendance.builder()
-                    .userId(attendanceRequestDto.getUserId())
-                    .clockInTime(currentTime)
-                    .present(true)
-                    .status("PENDING")
-                    .build();
-
-            attendanceRepository.save(newAttendance);
-        } catch (AlreadyMakeCheckInException alreadyMakeCheckInException) {
-            throw new AlreadyMakeCheckInException(alreadyMakeCheckInException.getMessage());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex.getMessage());
         }
+
+        Attendance newAttendance = Attendance.builder()
+                .userId(attendanceRequestDto.getUserId())
+                .clockInTime(currentTime)
+                .present(true)
+                .status("PENDING")
+                .build();
+
+        attendanceRepository.save(newAttendance);
     }
 
     @Transactional
     public void clockOut(AttendanceRequestDto attendanceRequestDto) {
-        try {
-            List<Attendance> attendances = findByUserId(attendanceRequestDto.getUserId());
+        List<Attendance> attendances = findByUserId(attendanceRequestDto.getUserId());
 
-            if (attendances.isEmpty()) {
-                throw new NotFoundException("Attendance not found to make check out");
-            }
-
-            Attendance lastAttendance = attendances.get(attendances.size() - 1);
-            LocalDateTime currentTime = LocalDateTime.now();
-
-            // Check if the user has already clocked out
-            if (lastAttendance.getClockOutTime() != null) {
-                throw new IllegalStateException("User has already clocked out for the day");
-            }
-
-            // check attendance check out is in the same day at check in last time
-            if (!lastAttendance.getClockInTime().toLocalDate().equals(currentTime.toLocalDate())) {
-                throw new NotFoundException("Attendance not found to make check out");
-            }
-
-            lastAttendance.setClockOutTime(currentTime);
-            lastAttendance.setPresent(false);
-            attendanceRepository.save(lastAttendance);
-
-            Duration duration = Duration.between(lastAttendance.getClockInTime(), currentTime);
-            String status = duration.toHours() >= 7 ? "COMPLETED" : "UNCOMPLETED";
-
-            Attendance newAttendance = Attendance.builder()
-                    .id(lastAttendance.getId())
-                    .userId(lastAttendance.getUserId())
-                    .clockInTime(lastAttendance.getClockInTime())
-                    .clockOutTime(currentTime)
-                    .present(true)
-                    .status(status)
-                    .build();
-
-            attendanceRepository.save(newAttendance);
-        } catch (NotFoundException notFoundException) {
-            throw new NotFoundException(notFoundException.getMessage());
-        } catch (Exception ex) {
-            throw new RuntimeException(ex.getMessage());
+        if (attendances.isEmpty()) {
+            throw new NotFoundException("Attendance not found to make check out");
         }
+
+        Attendance lastAttendance = attendances.get(attendances.size() - 1);
+        LocalDateTime currentTime = LocalDateTime.now();
+
+        // Check if the user has already clocked out
+        if (lastAttendance.getClockOutTime() != null) {
+            throw new IllegalStateException("User has already clocked out for the day");
+        }
+
+        // check attendance check out is in the same day at check in last time
+        if (!lastAttendance.getClockInTime().toLocalDate().equals(currentTime.toLocalDate())) {
+            throw new NotFoundException("Attendance not found to make check out");
+        }
+
+        lastAttendance.setClockOutTime(currentTime);
+        lastAttendance.setPresent(false);
+        attendanceRepository.save(lastAttendance);
+
+        Duration duration = Duration.between(lastAttendance.getClockInTime(), currentTime);
+        String status = duration.toHours() >= 7 ? "COMPLETED" : "UNCOMPLETED";
+
+        Attendance newAttendance = Attendance.builder()
+                .id(lastAttendance.getId())
+                .userId(lastAttendance.getUserId())
+                .clockInTime(lastAttendance.getClockInTime())
+                .clockOutTime(currentTime)
+                .present(true)
+                .status(status)
+                .build();
+
+        attendanceRepository.save(newAttendance);
+    }
+
+    @Transactional
+    public void delete(String id) {
+        Optional<Attendance> attendance = attendanceRepository.findById(id);
+
+        if (attendance.isEmpty()) {
+            throw new NotFoundException("Attendance not found to delete");
+        }
+        attendanceRepository.deleteById(id);
     }
 
     @Transactional(readOnly = true)
-    private List<Attendance> findByUserId(String userId) {
+    public List<Attendance> findByUserId(String userId) {
         return attendanceRepository.findByUserId(userId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Attendance> findAll() {
+        return attendanceRepository.findAll();
     }
 }
